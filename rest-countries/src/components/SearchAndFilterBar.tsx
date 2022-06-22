d6ba6ab60ea6641d4dc5718cb4a0a0e5f9cb4fea
Yarden-Tal <83368934+Yarden-Tal@users.noreply.github.com>
2022-06-22 23:29:43 +0300

    Sass impots improved; search & filter bar start

--- rest-countries/src/components/SearchAndFilterBar.tsx
+++ rest-countries/src/components/SearchAndFilterBar.tsx
@@ -1,12 +1,22 @@
+import searchSvg from "../assets/search.svg";
+import "../styles/searchAndFilterBar.scss";
+
 const SearchAndFilterBar = () => {
   return (
     <div className="search-filter-wrapper">
       <div className="search-wrapper">
-        <input type="text" name="search" />
-        <img src="" alt="search" className="seach-icon" />
+        <img src={searchSvg} alt="search" className="search-icon" />
+        <input
+          type="text"
+          name="search"
+          placeholder="Search for a country..."
+        />
       </div>
       <div className="filter-wrapper">
         <select name="region">
+          <option value="" disabled selected hidden>
+            Filter by Region
+          </option>
           <option value="africa">Africa</option>
           <option value="americas">Americas</option>
           <option value="asia">Asia</option>
